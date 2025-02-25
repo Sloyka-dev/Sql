16f4438da7e1623a1141376c5d40dabc587f00fc
Sloyka-dev <kuznetzovmarsel@yandex.ru>
2025-02-25 19:03:43 +0300

    Уязвимая версия программы

--- sql/Program.cs
+++ sql/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System.Text;
+using MySql.Data.MySqlClient;
+
+string constring = "Server=localhost; database=test; uid=root; pwd=root";
+
+using (MySqlConnection con = new (constring))
+{
+
+    con.Open();
+
+    while (true)
+    {
+
+        Console.Write("Введите логин: ");
+        string? login = Console.ReadLine();
+        Console.Write("Введите пароль: ");
+        string? pass = Console.ReadLine();
+
+        string req = $"select * from users where login = '{login}' and pass = '{pass}'";
+
+        using (MySqlCommand cmd = new MySqlCommand(req, con))
+        {
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            StringBuilder bu = new();
+            while (reader.Read())
+            {
+                bu.Append($"{reader.GetInt32(0)}. login: {reader.GetString(1)}; pass: {reader.GetString(2)}\n");
+            }
+
+            Console.WriteLine(bu.ToString());
+
+        }
+    
+    }
+
+}
