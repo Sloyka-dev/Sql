758837ed968eb2f4ec8f9e3e58d868efb0b06fc0
Sloyka-dev <kuznetzovmarsel@yandex.ru>
2025-02-25 19:58:53 +0300

    Защищенная версия

--- sql/Program.cs
+++ sql/Program.cs
@@ -3,23 +3,25 @@ using MySql.Data.MySqlClient;
 
 string constring = "Server=localhost; database=test; uid=root; pwd=root";
 
-using (MySqlConnection con = new (constring))
+while (true)
 {
 
-    con.Open();
+    Console.Write("Введите логин: ");
+    string? login = Console.ReadLine();
+    Console.Write("Введите пароль: ");
+    string? pass = Console.ReadLine();
 
-    while (true)
+    using (MySqlConnection con = new(constring))
     {
 
-        Console.Write("Введите логин: ");
-        string? login = Console.ReadLine();
-        Console.Write("Введите пароль: ");
-        string? pass = Console.ReadLine();
-
-        string req = $"select * from users where login = '{login}' and pass = '{pass}'";
-
+        con.Open();
+        string req = $"select * from users where login = @login and pass = @pass";
+        
         using (MySqlCommand cmd = new MySqlCommand(req, con))
         {
+
+            cmd.Parameters.Add(new MySqlParameter("@login", MySqlDbType.String) { Value = login });
+            cmd.Parameters.Add(new MySqlParameter("@pass", MySqlDbType.String) { Value = pass });
 
             MySqlDataReader reader = cmd.ExecuteReader();
             StringBuilder bu = new();
@@ -31,7 +33,7 @@ using (MySqlConnection con = new (constring))
             Console.WriteLine(bu.ToString());
 
         }
-    
+
     }
 
 }
